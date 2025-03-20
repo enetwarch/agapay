58b685aea8a7afb655826ee6174f8a6c77fd2db3
Enetwarch <enetwarch@protonmail.com>
2025-03-21 00:49:09 +0800

    added basic input validation for the sign-up page

--- js/sign-up.js
+++ js/sign-up.js
@@ -4,10 +4,42 @@ backArrowElement.addEventListener("click", () => {
 });
 
 const signUpFormElement = document.getElementById("signUpForm");
+const firstName = document.getElementById("firstName");
+const middleName = document.getElementById("middleName");
+const lastName = document.getElementById("lastName");
+const phoneNumber = document.getElementById("phoneNumber");
+const password = document.getElementById("password");
+const confirmPassword = document.getElementById("confirmPassword");
+
+phoneNumber.addEventListener("input", () => {
+    phoneNumber.setCustomValidity("");
+    // According to https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s03.html.
+    // Internationally, phone numbers have a maximum of 15 characters.
+    const maxLength = 15;
+    if (phoneNumber.value.length > maxLength) {
+        phoneNumber.value = phoneNumber.value.slice(0, maxLength);
+    }
+});
+
+confirmPassword.addEventListener("input", () => {
+    confirmPassword.setCustomValidity("");
+});
+
 const signUpModalElement = document.getElementById("signUpModal");
 signUpFormElement.addEventListener("submit", event => {
     event.preventDefault();
-    signUpModalElement.showModal();
+    // According to https://www.oreilly.com/library/view/regular-expressions-cookbook/9781449327453/ch04s03.html.
+    // Internationally, phone numbers have a minimum of 7 characters.
+    if (phoneNumber.value.length < 7) {
+        phoneNumber.setCustomValidity("Minimum phone number character length is 7.");
+    } else if (confirmPassword.value !== password.value) {
+        confirmPassword.setCustomValidity("Passwords do not match.");
+    }
+    if (signUpFormElement.checkValidity()) {
+        signUpModalElement.showModal();
+    } else {
+        signUpFormElement.reportValidity();
+    }
 });
 
 const closeModalElement = document.getElementById("closeModal");
