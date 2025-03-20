fc060d9fa812a29b5d098fe5f5685f94b0d8f3e4
Enetwarch <enetwarch@protonmail.com>
2025-03-20 22:56:48 +0800

    added basic authentication modal for sign-up

--- js/sign-up.js
+++ js/sign-up.js
@@ -2,3 +2,15 @@ const backArrowElement = document.getElementById("backArrow");
 backArrowElement.addEventListener("click", () => {
     window.location.href = "./";
 });
+
+const signUpFormElement = document.getElementById("signUpForm");
+const signUpModalElement = document.getElementById("signUpModal");
+signUpFormElement.addEventListener("submit", event => {
+    event.preventDefault();
+    signUpModalElement.showModal();
+});
+
+const closeModalElement = document.getElementById("closeModal");
+closeModalElement.addEventListener("click", () => {
+    signUpModalElement.close();
+});
