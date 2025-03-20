5187b4fe3c958f20a3ce16b1f4cee39d331aedc2
Enetwarch <enetwarch@protonmail.com>
2025-03-21 04:53:21 +0800

    added password view functionality to sign-up page

--- js/log-in.js
+++ js/log-in.js
@@ -1,13 +1,51 @@
-import Password from "./modules/password.js";
-
-window.addEventListener("load", () => {
-    new Password(document.getElementById("password"), "log-in");
-});
+const htmlLocation = "log-in";
 
 const backArrowElement = document.getElementById("backArrow");
 backArrowElement.addEventListener("click", () => {
     window.location.href = "./";
 });
+
+function Password(input) {
+    if (!new.target) {
+        throw Error(`Use the "new" keyword on the Password constructor.`);
+    }
+    this.input = input;
+    const view = document.createElement("i");
+    view.classList.add(`${htmlLocation}__password-view`, "fa-solid", "fa-eye");
+    this.view = view;
+    this.open = false;
+    this.addListeners();
+}
+
+Password.prototype.addListeners = function() {
+    this.input.addEventListener("input", () => this.attachView());
+    this.view.addEventListener("click", () => this.toggleView());
+}
+
+Password.prototype.attachView = function() {
+    if (this.input.value.length > 0) {
+        this.input.after(this.view);
+    } else {
+        this.view.remove();
+        if (this.open) this.toggleView();
+    }
+}
+
+Password.prototype.toggleView = function() {
+    if (this.open) {
+        this.input.type = "password";
+        this.view.classList.remove("fa-eye-slash");
+        this.view.classList.add("fa-eye");
+        this.open = false;
+    } else {
+        this.input.type = "text";
+        this.view.classList.remove("fa-eye");
+        this.view.classList.add("fa-eye-slash");
+        this.open = true;
+    }
+}
+
+new Password(document.getElementById("password"));
 
 const phoneNumber = document.getElementById("phoneNumber");
 phoneNumber.addEventListener("input", () => {
@@ -19,7 +57,6 @@ phoneNumber.addEventListener("input", () => {
         phoneNumber.value = phoneNumber.value.slice(0, maxLength);
     }
 });
-
 
 const logInFormElement = document.getElementById("logInForm");
 const logInModalElement = document.getElementById("logInModal");
--- js/sign-up.js
+++ js/sign-up.js
@@ -1,3 +1,5 @@
+const htmlLocation = "sign-up";
+
 const backArrowElement = document.getElementById("backArrow");
 backArrowElement.addEventListener("click", () => {
     window.location.href = "./";
@@ -17,6 +19,49 @@ phoneNumber.addEventListener("input", () => {
         phoneNumber.value = phoneNumber.value.slice(0, maxLength);
     }
 });
+
+function Password(input) {
+    if (!new.target) {
+        throw Error(`Use the "new" keyword on the Password constructor.`);
+    }
+    this.input = input;
+    const view = document.createElement("i");
+    view.classList.add(`${htmlLocation}__password-view`, "fa-solid", "fa-eye");
+    this.view = view;
+    this.open = false;
+    this.addListeners();
+}
+
+Password.prototype.addListeners = function() {
+    this.input.addEventListener("input", () => this.attachView());
+    this.view.addEventListener("click", () => this.toggleView());
+}
+
+Password.prototype.attachView = function() {
+    if (this.input.value.length > 0) {
+        this.input.after(this.view);
+    } else {
+        this.view.remove();
+        if (this.open) this.toggleView();
+    }
+}
+
+Password.prototype.toggleView = function() {
+    if (this.open) {
+        this.input.type = "password";
+        this.view.classList.remove("fa-eye-slash");
+        this.view.classList.add("fa-eye");
+        this.open = false;
+    } else {
+        this.input.type = "text";
+        this.view.classList.remove("fa-eye");
+        this.view.classList.add("fa-eye-slash");
+        this.open = true;
+    }
+}
+
+new Password(document.getElementById("password"));
+new Password(document.getElementById("confirmPassword"));
 
 confirmPassword.addEventListener("input", () => {
     confirmPassword.setCustomValidity("");
@@ -49,7 +94,6 @@ closeModalElement.addEventListener("click", () => {
 });
 
 const authNumbers = document.querySelectorAll("#authNumbers > *");
-console.log(authNumbers);
 authNumbers.forEach(number => {
     const maxLength = 1;
     number.addEventListener("input", () => {
