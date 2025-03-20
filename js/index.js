10707d5ad4b367058ad764da7a51471a781b4fdd
Enetwarch <enetwarch@protonmail.com>
2025-03-20 22:58:03 +0800

    added a way to get to sign-up

--- js/index.js
+++ js/index.js
@@ -1,3 +1,8 @@
+const signUpElement = document.getElementById("signUp");
+signUpElement.addEventListener("click", () => {
+    window.location.href = "sign-up";
+});
+
 const logInElement = document.getElementById("logIn");
 logInElement.addEventListener("click", () => {
     window.location.href = "log-in";
