ad32ed5150c1617874dd45751be43545abcee556
Enetwarch <enetwarch@protonmail.com>
2025-04-04 01:29:44 +0800

    separated initialization of ui to ui.js controller

--- js/main.js
+++ js/main.js
@@ -1,10 +1,5 @@
-import elements from "./data/elements.json" with { "type": "json" };
-import Code from "./modules/code.js";
 import Warning from "./modules/warning.js";
-import Form from "./modules/form.js";
-import Modal from "./modules/modal.js";
-import Fields from "./modules/fields.js";
-import Entries from "./modules/entries.js";
+import UI from "./controllers/ui.js";
 
 document.addEventListener("contextmenu", event => event.preventDefault());
 
@@ -14,116 +9,7 @@ window.addEventListener("load", () => {
     const message = "Please view on mobile";
 
     const warning = new Warning(media, iconClass, message);
+
+    const page = document.body.dataset.page;
+    const ui = new UI(page);
 });
-
-const page = document.body.dataset.page;
-
-switch (page) {
-
-    case "sign-up": {
-        const modalElement = document.getElementById("modal");
-        const modal = new Modal(modalElement);
-
-        const formElement = document.getElementById("form");
-        const form = new Form(formElement);
-        form.onSubmit(() => {
-            const phoneNumber = document.getElementById("phoneNumber");
-            const phoneNumberModal = document.getElementById("phoneNumberModal");
-            phoneNumberModal.innerText = phoneNumber.value;
-
-            modal.show();
-        });
-
-        const resendCodeElement = document.getElementById("resendCode");
-        const resendCode = new Code(resendCodeElement);
-
-        const successModalElement = document.getElementById("successModal");
-        const successModal = new Modal(successModalElement);
-
-        const modalFormElement = document.getElementById("modalForm");
-        const modalForm = new Form(modalFormElement);
-        modalForm.onSubmit(() => {
-            modal.close();
-            successModal.show();
-            setTimeout(() => window.location.href = "log-in", 2000);
-        });
-
-        break;
-    }
-
-    case "log-in": {
-        const modalElement = document.getElementById("modal");
-        const modal = new Modal(modalElement);
-
-        const formElement = document.getElementById("form");
-        const form = new Form(formElement);
-        form.onSubmit(() => {
-            modal.show();
-            setTimeout(() => window.location.href = "home", 2000);
-        });
-
-        break;
-    }
-
-    case "forgot-password": {
-        const modalElement = document.getElementById("modal");
-        const modal = new Modal(modalElement);
-
-        const successModalElement = document.getElementById("successModal");
-        const successModal = new Modal(successModalElement);
-
-        const formElement = document.getElementById("form");
-        const form = new Form(formElement);
-        form.onSubmit(() => {
-            const phoneNumber = document.getElementById("phoneNumber");
-            const phoneNumberModal = document.getElementById("phoneNumberModal");
-            phoneNumberModal.innerText = phoneNumber.value;
-
-            modal.show();
-        });
-
-        const resendCodeElement = document.getElementById("resendCode");
-        const resendCode = new Code(resendCodeElement);
-
-        const modalFormElement = document.getElementById("modalForm");
-        const modalForm = new Form(modalFormElement);
-        modalForm.onSubmit(() => {
-            modal.close();
-            successModal.show();
-            setTimeout(() => window.location.href = "log-in", 2000);
-        });
-
-        break;
-    }
-
-    case "home": {
-        const actionContainer = document.getElementById("actionContainer");
-        const fields = new Fields("action", actionContainer, elements.fields.actions);
-        break;
-    }
-
-    case "inbox": {
-        const inboxContainer = document.getElementById("inboxContainer");
-        const entries = new Entries("inbox", inboxContainer, elements.entries.inbox);
-        break;
-    }
-
-    case "profile": {
-        const menuContainer = document.getElementById("menuContainer");
-        const fields = new Fields("menu", menuContainer, elements.fields.menu);
-        break;
-    }
-
-}
-
-switch (page) {
-    case "home":
-    case "cards":
-    case "scan":
-    case "inbox":
-    case "profile": {
-        const navContainer = document.getElementById("navContainer");
-        const fields = new Fields("nav", navContainer, elements.fields.nav);
-        break;
-    }
-}
