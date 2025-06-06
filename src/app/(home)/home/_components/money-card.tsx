d323435cde41e838d7afdb4e85df4cb82aefe913
Enetwarch <enetwarch@protonmail.com>
2025-06-06 20:03:24 +0800

    added sizes to the money and badge cards

--- src/app/(home)/home/_components/money-card.tsx
+++ src/app/(home)/home/_components/money-card.tsx
@@ -5,13 +5,13 @@ export default function MoneyCard({ className, ...props }: MoneyCardProps): Reac
   return (
     <article
       className={cn(
-        "relative flex flex-col overflow-hidden rounded-xl bg-primary text-background text-card-foreground shadow-black/30 shadow-md transition-opacity duration-200 active:opacity-70",
+        "relative flex h-50 w-full flex-col gap-4 overflow-hidden rounded-xl bg-primary p-4 text-background text-card-foreground shadow-black/30 shadow-md transition-opacity duration-200 active:opacity-70",
         className,
       )}
       {...props}
     >
       <div className="absolute top-4 left-4 flex flex-col">
-        <h3 className="font-bold text-3xl">$69420</h3>
+        <h3 className="font-bold text-3xl text-background">$69420</h3>
       </div>
     </article>
   );
