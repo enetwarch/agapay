47bd909d6a79e69a6819ff99def94a4339f29c37
Enetwarch <enetwarch@protonmail.com>
2025-06-10 17:50:39 +0800

    chore: improve immutability of constants

--- src/constants/metadata.ts
+++ src/constants/metadata.ts
@@ -1,58 +1,52 @@
-import type { Pathname } from "./pathnames";
+import type { StaticPathname } from "./pathnames";
 
 import type { Metadata } from "next";
 
-export const index: Metadata = Object.freeze({
-  title: "Agapay | Tap to Prosper",
-  description:
-    "Welcome to Agapay, an e-wallet inspired idle clicker game. Start building your wealth and prestige today!",
+// Only records static pathnames because dynamic paths will have varying metadata.
+const metadata: Record<StaticPathname, Metadata> = Object.freeze({
+  "/": Object.freeze({
+    title: "Agapay | Tap to Prosper",
+    description:
+      "Welcome to Agapay, an e-wallet inspired idle clicker game. Start building your wealth and prestige today!",
+  }),
+  "/login": Object.freeze({
+    title: "Agapay | Log In",
+    description: "Already a member? Log in to continue your progress and earnings in Agapay.",
+  }),
+  "/signup": Object.freeze({
+    title: "Agapay | Sign Up",
+    description: "Join Agapay today! Create your account and begin your tapping journey to success.",
+  }),
+  "/home": Object.freeze({
+    title: "Agapay | Home",
+    description:
+      "Tap your way to riches! Earn money, unlock upgrades, and boost your income in this addictive clicker game.",
+  }),
+  "/prestige": Object.freeze({
+    title: "Agapay | Prestige",
+    description:
+      "Reset your progress for massive and permanent long-term gains. Prestige to multiply your income and climb faster.",
+  }),
+  "/support": Object.freeze({
+    title: "Agapay | Support",
+    description: "Consider supporting this indie web project!",
+  }),
+  "/inbox": Object.freeze({
+    title: "Agapay | Inbox",
+    description: "Check your messages, rewards, and announcements from Agapay.",
+  }),
+  "/profile": Object.freeze({
+    title: "Agapay | Profile",
+    description: "View and manage your account, track your stats, and personalize your Agapay experience.",
+  }),
 });
 
-export const login: Metadata = Object.freeze({
-  title: "Agapay | Log In",
-  description: "Already a member? Log in to continue your progress and earnings in Agapay.",
-});
+export const index: Metadata = Object.freeze(metadata["/"]);
+export const login: Metadata = Object.freeze(metadata["/login"]);
+export const signup: Metadata = Object.freeze(metadata["/signup"]);
 
-export const signup: Metadata = Object.freeze({
-  title: "Agapay | Sign Up",
-  description: "Join Agapay today! Create your account and begin your tapping journey to success.",
-});
-
-export const home: Metadata = Object.freeze({
-  title: "Agapay | Home",
-  description:
-    "Tap your way to riches! Earn money, unlock upgrades, and boost your income in this addictive clicker game.",
-});
-
-export const prestige: Metadata = Object.freeze({
-  title: "Agapay | Prestige",
-  description:
-    "Reset your progress for massive and permanent long-term gains. Prestige to multiply your income and climb faster.",
-});
-
-export const support: Metadata = Object.freeze({
-  title: "Agapay | Support",
-  description: "Consider supporting this indie web project!",
-});
-
-export const inbox: Metadata = Object.freeze({
-  title: "Agapay | Inbox",
-  description: "Check your messages, rewards, and announcements from Agapay.",
-});
-
-export const profile: Metadata = Object.freeze({
-  title: "Agapay | Profile",
-  description: "View and manage your account, track your stats, and personalize your Agapay experience.",
-});
-
-/** @internal this is exported, but it should only be used to make sure all pages have metadata through the Record type. */
-const _metadata: Record<Pathname, Metadata> = Object.freeze({
-  "/": index,
-  "/login": login,
-  "/signup": signup,
-  "/home": home,
-  "/prestige": prestige,
-  "/support": support,
-  "/inbox": inbox,
-  "/profile": profile,
-});
+export const home: Metadata = Object.freeze(metadata["/home"]);
+export const prestige: Metadata = Object.freeze(metadata["/prestige"]);
+export const support: Metadata = Object.freeze(metadata["/support"]);
+export const inbox: Metadata = Object.freeze(metadata["/inbox"]);
+export const profile: Metadata = Object.freeze(metadata["/profile"]);
--- src/constants/pathnames.ts
+++ src/constants/pathnames.ts
@@ -1,43 +1,42 @@
 // Add more pathnames if necessary to maintain typesafety.
 
-export type RootPathname = (typeof rootPathnames)[number];
-export const rootPathnames = [
-  "/", // @/app/page.tsx
-] as const;
-
-export function isRootPathname(value: string): value is RootPathname {
-  return rootPathnames.includes(value as RootPathname);
-}
-
+export const authPathnames = ["/", "/login", "/signup"] as const;
 export type AuthPathname = (typeof authPathnames)[number];
-export const authPathnames = [
-  "/login", // @/app/(auth)/login/page.tsx
-  "/signup", // @/app/(auth)/signup/page.tsx
-] as const;
 
 export function isAuthPathname(value: string): value is AuthPathname {
   return authPathnames.includes(value as AuthPathname);
 }
 
+export const tabPathnames = ["/home", "/prestige", "/support", "/inbox", "/profile"] as const;
 export type TabPathname = (typeof tabPathnames)[number];
-export const tabPathnames = [
-  "/home", // @/app/(tabs)/home/page.tsx
-  "/prestige", // @/app/(tabs)/prestige/page.tsx
-  "/support", // @/app/(tabs)/support/page.tsx
-  "/inbox", // @/app/(tabs)/inbox/page.tsx
-  "/profile", // @/app/(tabs)/profile/page.tsx
-] as const;
 
 export function isTabPathname(value: string): value is TabPathname {
   return tabPathnames.includes(value as TabPathname);
 }
 
+export const unprotectedPathnames = [...authPathnames] as const;
+export type UnprotectedPathname = (typeof unprotectedPathnames)[number];
+
+export function isUnprotectedPathname(value: string): value is UnprotectedPathname {
+  return unprotectedPathnames.includes(value as UnprotectedPathname);
+}
+
+export const protectedPathnames = [...tabPathnames] as const;
+export type ProtectedPathname = (typeof protectedPathnames)[number];
+
+export function isProtectedPathname(value: string): value is ProtectedPathname {
+  return protectedPathnames.includes(value as ProtectedPathname);
+}
+
+export const staticPathnames = [...authPathnames, ...tabPathnames] as const;
+export type StaticPathname = (typeof staticPathnames)[number];
+
+export function isStaticPathname(value: string): value is StaticPathname {
+  return staticPathnames.includes(value as StaticPathname);
+}
+
+export const pathnames = [...staticPathnames] as const;
 export type Pathname = (typeof pathnames)[number];
-export const pathnames = [
-  ...rootPathnames,
-  ...authPathnames, // @/app/(auth)/
-  ...tabPathnames, // @/app/(tabs)/
-] as const;
 
 export function isPathname(value: string): value is Pathname {
   return pathnames.includes(value as Pathname);
