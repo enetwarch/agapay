5b13d03941855b5554e98a7676261cfe1c3236a0
Enetwarch <enetwarch@protonmail.com>
2025-06-18 14:20:41 +0800

    refactor: replaced dynamic warning from js to htlm

--- js/main.js
+++ js/main.js
@@ -1,15 +1,8 @@
-import Warning from "./modules/warning.js";
 import UI from "./controllers/ui.js";
 
 document.addEventListener("contextmenu", event => event.preventDefault());
 
 window.addEventListener("load", () => {
-    const media = window.matchMedia("(max-width: 640px)");
-    const iconClass = "fa-mobile-screen";
-    const message = "Please view on mobile";
-
-    const warning = new Warning(media, iconClass, message);
-
     const page = document.body.dataset.page;
     const ui = new UI(page);
 });
