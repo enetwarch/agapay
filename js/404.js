1447cb41018df13a34d9951d33d768ce4c33afed
Enetwarch <enetwarch@protonmail.com>
2025-06-19 00:43:05 +0800

    fix: added trailing slash to 404 redirection

--- js/404.js
+++ js/404.js
@@ -1,7 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
   // GitHub pages will have a domain name based on the current repo.
-  // In this case, it would be "/agapay".
+  // In this case, it would be "/agapay/".
 
-  const timeoutId = setTimeout(() => window.location.href = "/agapay", 3000);
+  const timeoutId = setTimeout(() => window.location.href = "/agapay/", 3000);
   window.addEventListener("beforeunload", () => clearTimeout(timeoutId));
 });
