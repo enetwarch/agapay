3cb1054c3c0753a2940ee270d9febb2fdde5cc06
Enetwarch <enetwarch@protonmail.com>
2025-06-19 00:40:41 +0800

    fix: github pages redirection from 404 to index

--- js/404.js
+++ js/404.js
@@ -1,4 +1,7 @@
 window.addEventListener("DOMContentLoaded", () => {
-  const timeoutId = setTimeout(() => window.location.href = "/", 3000);
+  // GitHub pages will have a domain name based on the current repo.
+  // In this case, it would be "/agapay".
+
+  const timeoutId = setTimeout(() => window.location.href = "/agapay", 3000);
   window.addEventListener("beforeunload", () => clearTimeout(timeoutId));
 });
