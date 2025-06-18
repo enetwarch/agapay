4db63083137acc2cc49151a568f821dde2aa012a
Enetwarch <enetwarch@protonmail.com>
2025-06-18 20:54:31 +0800

    chore: deleted unused js files

--- js/main.js
+++ js/main.js
@@ -1,8 +1,1 @@
-import UI from "./controllers/ui.js";
-
 document.addEventListener("contextmenu", event => event.preventDefault());
-
-window.addEventListener("load", () => {
-    const page = document.body.dataset.page;
-    const ui = new UI(page);
-});
